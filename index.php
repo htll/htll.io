fd98b1f42b680680288571ddbf56f91ca88dd6ec
Suruga Devil <epierce118@gmail.com>
2015-04-04 10:00:45 -0400

    Import

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<title>Official HTLL Link Shortener</title>
+<meta charset="UTF-8" />
+<meta name="robots" content="noindex, nofollow">
+</html>
+<body>
+<form method="post" action="shorten.php" id="shortener">
+<label for="longurl">URL to shorten</label> <input type="text" name="longurl" id="longurl"> <input type="submit" value="Shorten">
+</form>
+</form>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script>
+$(function () {
+	$('#shortener').submit(function () {
+		$.ajax({data: {longurl: $('#longurl').val()}, url: 'shorten.php', complete: function (XMLHttpRequest, textStatus) {
+			$('#longurl').val(XMLHttpRequest.responseText);
+		}});
+		return false;
+	});
+});
+</script>
+</body>
+</html>
